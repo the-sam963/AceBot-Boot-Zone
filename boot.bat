e1121660b121eb6854337b82c75feaffba6fdf18
Samrat <samratbiswas986@gmail.com>
2022-07-11 06:01:26 +0530

    Initial commit of all files to the repository

--- boot.bat
+++ boot.bat
@@ -1,2 +1,5 @@
 @echo off
-powershell -Command "mkdir _Folder_From_Bat"
+powershell -Command "mkdir _Folder_From_Bat-1"
+powershell -Command "mkdir _Folder_From_Bat-2"
+powershell -Command "mkdir _Folder_From_Bat-3"
+powershell -Command "mkdir _Folder_From_Bat-4"
